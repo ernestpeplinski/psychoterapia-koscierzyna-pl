ce417b17938a5ed9792534f242ae373cea5737ee
Ernest Peplinski <ernestpeplinski@gmail.com>
2025-11-26 17:01:15 +0100

    Initialize React project with Vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Psychoterapia Kościerzyna</h1>
+      <p>Strona w budowie, będzie dostępna wkrótce</p>
+    </div>
+  );
+}
+
+export default App;
