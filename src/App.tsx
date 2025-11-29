190675a83400d1238a15f00599803859e954823e
Ernest Peplinski <ernestpeplinski@gmail.com>
2025-11-29 17:17:31 +0100

    Add Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.scss";
+import logo from "./assets/Logo 1.png";
 
 function App() {
   return (
     <div className="container">
+      <img src={logo} alt="Logo Psychoterapia Kościerzyna" className="logo" />
       <h1>Psychoterapia Kościerzyna</h1>
       <p>Strona w budowie, będzie dostępna wkrótce</p>
     </div>
