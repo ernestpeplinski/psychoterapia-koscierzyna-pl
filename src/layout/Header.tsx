3383c9fcc0c84e3379f234febbde143740315766
Ernest Peplinski <ernestpeplinski@gmail.com>
2025-11-29 19:18:32 +0100

    Create header page

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,14 +1,46 @@
 import { NavLink } from "react-router-dom";
+import "../styles/header.scss";
+import logo from "../assets/Logo 1.png";
 
 function Header() {
   return (
-    <header>
-      <h1>Header</h1>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/o-mnie">O mnie</NavLink>
-        <NavLink to="/cennik">Cennik</NavLink>
-        <NavLink to="/kontakt">Kontakt</NavLink>
+    <header className="header">
+      <div className="header__left">
+        <div className="header__logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="header__brand">
+          <h1 className="header__title">Psychoterapia Kościerzyna</h1>
+          <p className="header__subtitle">
+            Psychoterapia i wsparcie Magdalena Milewczyk
+          </p>
+        </div>
+      </div>
+      <nav className="header__nav">
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Strona główna
+        </NavLink>
+        <NavLink
+          to="/o-mnie"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          O mnie
+        </NavLink>
+        <NavLink
+          to="/cennik"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Cennik
+        </NavLink>
+        <NavLink
+          to="/kontakt"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Kontakt
+        </NavLink>
       </nav>
     </header>
   );
