8211f14606f7aa5d11b9d7a3508621688ec12a20
Ernest Peplinski <ernestpeplinski@gmail.com>
2025-11-29 20:12:24 +0100

    Create NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+import "../styles/notFound.scss";
+import { FaHome, FaExclamationTriangle } from "react-icons/fa";
+
 function NotFound() {
   return (
-    <div>
-      <h1>404 - Not Found</h1>
+    <div className="not-found">
+      <div className="not-found__container">
+        <div className="not-found__icon">
+          <FaExclamationTriangle />
+        </div>
+        <h1 className="not-found__title">404</h1>
+        <h2 className="not-found__subtitle">Strona nie została znaleziona</h2>
+        <p className="not-found__description">
+          Przepraszamy, ale strona której szukasz nie istnieje.
+        </p>
+        <Link to="/" className="not-found__button">
+          <FaHome />
+          <span>Wróć do strony głównej</span>
+        </Link>
+      </div>
     </div>
   );
 }
